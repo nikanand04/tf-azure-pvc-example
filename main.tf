9ee817a622fbdec10ac807e0ea542e29e12f84fa
Nikita Anand <51712876+nikanand04@users.noreply.github.com>
2021-04-27 10:21:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,13 +11,34 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "aks" {
+  backend = "remote"
+  config  = {
+    organization = "nikita_hashi"
+    workspaces   = {
+      name       = "terraform-provision-aks-cluster"
+    }
+  }
+}
+
+# Retrieve AKS cluster information
 provider "azurerm" {
   features {}
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = data.terraform_remote_state.aks.outputs.kubernetes_cluster_name
+  resource_group_name = data.terraform_remote_state.aks.outputs.resource_group_name
 }
+
+provider "kubernetes" {
+  host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+}
+
 module "storage" {
   source = "./storage"
 }
