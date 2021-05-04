7a695deffc0a7aafb4a1aabd7eadbb774955bb41
Nikita Anand <51712876+nikanand04@users.noreply.github.com>
2021-05-04 11:43:25 -0400

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -59,16 +59,16 @@ resource "kubernetes_deployment" "my_app_deployment" {
             }
           }
 
-          liveness_probe {
-            http_get {
-              path = "/nginx_status"
-              port = 80
+#           liveness_probe {
+#             http_get {
+#               path = "/nginx_status"
+#               port = 80
 
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
+#               http_header {
+#                 name  = "X-Custom-Header"
+#                 value = "Awesome"
+#               }
+#             }
 
             initial_delay_seconds = 3
             period_seconds        = 3
